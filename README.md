1b03c25437a47465cb8adc91fbdde6e0e39b70c6
gum3ng <gum3ng@protonmail.com>
2021-12-28 20:02:29 +0530

    solution to wcat

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # osstep-solutions
+This repo contains the solutions to the projects of the operating systems book ostep. The questions can be found [here](https://github.com/remzi-arpacidusseau/ostep-projects)
---
+++ initial-utilities/wcat.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+int main(int argc, char **argv){
+  int i;
+  for(i=0; i<argc-1;++i){
+    FILE *fp = fopen(argv[i+1], "r");
+    if (fp == NULL) {
+        printf("wcat: cannot open file\n");
+        exit(1);
+    }
+    // get the file size
+    fseek(fp, 0L, SEEK_END);
+    // calculating the size of the file
+    long long size = ftell(fp);
+    rewind(fp);
+    char *str = calloc(1, size+1);
+    if (!str) {
+      fclose(fp);
+      printf("\nBuffer allocation failed");
+      exit(1);
+    }
+    fread(str, size, 1, fp);
+    printf("%s",str);
+    fclose(fp);
+    //printf("\n");
+  }
+  return 0;
+}
